ce08a33be2ceac91e158705427600c079d283dd3
KenishRaghu <kenishr08@gmail.com>
2025-06-18 16:32:01 -0400

    Initial commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Kenish Raghu on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
